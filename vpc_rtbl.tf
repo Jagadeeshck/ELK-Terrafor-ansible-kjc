c1afbc9deabd3369ecce49df4078a4919ab2d51b
jagadeesh CHandra K <jagadeeshchandra90@gmail.com>
2025-02-27 22:48:36 +0000

    test

--- vpc_rtbl.tf
+++ vpc_rtbl.tf
@@ -1,8 +1,8 @@
 module "routable_vpc" {
-  source       = "./modules/vpc_network"
-  vpc_name     = "routable-vpc"
-  cidr_block   = "10.0.0.0/16"
-  is_routable  = true
-  aws_region   = var.aws_region
-  az_count     = var.az_count
+  source      = "./modules/vpc_network"
+  vpc_name    = "routable-vpc"
+  cidr_block  = "10.0.0.0/16"
+  is_routable = true
+  aws_region  = var.aws_region
+  az_count    = var.az_count
 }
