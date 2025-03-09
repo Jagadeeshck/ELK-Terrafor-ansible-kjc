cd863959d134ebe28e8360bc5395d47daf449149
jagadeesh CHandra K <jagadeeshchandra90@gmail.com>
2025-03-09 22:09:50 +0000

    updated dns

--- dns.tf
+++ dns.tf
@@ -1,5 +1,5 @@
 data "aws_route53_zone" "main" {
-  count        = var.domain_name != "" ? 1 : 0
-  name         = "${var.domain_name}."
+  count       = var.domain_name != "" ? 1 : 0
+  name        = "${var.domain_name}."
   private_zone = false
 }
